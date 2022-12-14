65c3e9ada9155b3a3b7c959c92bf62581aa28dee
Ritanshu Yadav <ritanshuyadav@gmail.com>
2022-12-14 23:39:17 +0530

    Add project files.

---
+++ AppointmentScheduling/Models/ViewModels/CommonResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AppointmentScheduling.Models.ViewModels
+{
+    public class CommonResponse<T>
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+
+        public T dataenum { get; set; }
+    }
+}
